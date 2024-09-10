85c309883d1e3a9001a69aa1578490a2e3795c8e
stas4964035 <65187168+stas4964035@users.noreply.github.com>
2024-09-10 15:03:07 +0300

    Add files via upload

---
+++ SpringBoot/src/main/java/task/Applications.java
@@ -0,0 +1,11 @@
+package task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applications {
+    public static void main(String[] args) {
+        SpringApplication.run(Applications.class);
+    }
+}
